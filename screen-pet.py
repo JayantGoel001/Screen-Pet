48891a636e2ddcf044c39b6203520cc841ca3241
username <54479676+CormacKrum@users.noreply.github.com>
2020-12-17 19:21:59 +0530

    Initial Commit

--- screen-pet.py
+++ screen-pet.py
@@ -1,7 +1,24 @@
 from tkinter import *
 
 win = Tk()
-c = Canvas(win, "dark blue", height=400, width=400)
+c = Canvas(win, height=400, width=400)
+c.configure(bg='dark blue', highlightthickness=0)
 
+c.body_color = "SkyBlue1"
+body = c.create_oval(35, 20, 365, 350, outline=c.body_color, fill=c.body_color)
+
+foot_left = c.create_oval(65, 320, 145, 360, outline=c.body_color, fill=c.body_color)
+foot_right = c.create_oval(250, 320, 330, 360, outline=c.body_color, fill=c.body_color)
+
+ear_left = c.create_polygon(75, 80, 75, 10, 165, 70, outline=c.body_color, fill=c.body_color)
+ear_right = c.create_polygon(255, 45, 325, 10, 320, 70, outline=c.body_color, fill=c.body_color)
+
+eye_left = c.create_oval(130, 110, 160, 170, outline='black', fill='white')
+pupil_left = c.create_oval(140, 145, 150, 155, outline='black', fill='black')
+
+eye_right = c.create_oval(230, 110, 260, 170, outline='black', fill='white')
+pupil_right = c.create_oval(240, 145, 250, 155, outline='black', fill='black')
+
+c.pack()
 
 win.mainloop()
